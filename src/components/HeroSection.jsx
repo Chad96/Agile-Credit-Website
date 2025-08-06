c86af437e65efc13e19b1cb31fe80aae4dc574b7
Chad96 <chadrackndalamba@gmail.com>
2025-08-06 02:07:51 +0200

    fixed the contact form

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,7 +3,7 @@ import './HeroSection.css';
 
 const HeroSection = () => (
   <section className="hero-section-with-img">
-    <img src="/images/her.jpg" alt="Hero" className="hero-img" />
+    <img src="/images/bg1.jpg" alt="Hero" className="hero-img" />
     <div className="hero-content">
       <h1 className="display-2 fw-bold text-light mb-4">
         Your Journey From Debt to Prosperity Starts Here
